9a1778d8fa96d0c820389cb8628c0e5df425df61
Asha <akarim@ucsc.edu>
2015-05-19 11:30:00 -0700

    9-5-2

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -1,22 +1,23 @@
 Package.describe({
-  name: 'tmeasday:errors',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  name: "tmeasday:errors",
+  summary: "A pattern to display application errors to the user",
+  version: "1.0.0"
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('errors.js');
+Package.onUse(function (api, where) {
+  api.versionsFrom('0.9.0');
+
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+
+  if (api.export) 
+    api.export('Errors');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('tmeasday:errors');
-  api.addFiles('errors-tests.js');
+  api.use('tmeasday:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');  
+
+  api.addFiles('errors_tests.js', 'client');
 });
