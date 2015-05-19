78c8d863b95b131b03f38a8f77215d69c7e41d8f
Asha <akarim@ucsc.edu>
2015-05-19 11:28:08 -0700

    9-5-1

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,15 @@
+Template.postEdit.onCreated(function() {
+  Session.set('postEditErrors', {});
+});
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -9,10 +21,19 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url)
+      return Session.set('postEditErrors', errors);
+
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
-        throwError(error.reason);
+
+         // show this result but route anyway
+  if (result.postExists)
+    Errors.throw('This link has already been posted');
+
+    Errors.throw(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
