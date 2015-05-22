209eb7514191965c19eda5d04ff7fa8261a0f6b0
Asha <akarim@ucsc.edu>
2015-05-21 22:28:42 -0700

    13-1

--- server/publications.js
+++ server/publications.js
@@ -7,13 +7,16 @@ Meteor.publish('posts', function(options) {
 });
 
 Meteor.publish('singlePost', function(id) {
-  check(id, String)
+  check(id, String);
   return Posts.find(id);
+});
+
 
 Meteor.publish('comments', function(postId) {
-	check(postId, String);
+  check(postId, String);
   return Comments.find({postId: postId});
 });
+
 Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId, read: false});
 });
